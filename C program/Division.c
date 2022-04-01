c55c4756bc4469da6383eabcb00d530d94d650b2
nehasontakke20 <nishantsontakke2801@gmail.com>
2022-04-01 09:57:50 +0530

    divison

---
+++ C program/Division.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float num1,num2,res;
+	printf("Enter any two numbers : ");
+	scanf("%f %f",&num1,&num2);
+	res=num1/num2;
+	printf("Division is = %f",res);
+	
+	
+}
+
+
