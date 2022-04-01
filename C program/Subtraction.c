2426e152b11e6d545ce42a74cebdc8c9b114a495
nehasontakke20 <nishantsontakke2801@gmail.com>
2022-04-01 10:05:04 +0530

    subtraction

---
+++ C program/Subtraction.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num1,num2,res;
+	printf("Enter any two numbers : ");
+	scanf("%d %d",&num1,&num2);
+	res=num1-num2;
+	printf("Subtraction is = %d",res);
+	
+	
+}
+
+
