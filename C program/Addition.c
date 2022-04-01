1629d24d484851f18c490c2b994f72acccf017f7
nehasontakke20 <nishantsontakke2801@gmail.com>
2022-04-01 09:41:40 +0530

    C programs

---
+++ C program/Addition.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num1,num2,res;
+	printf("Enter any two numbers : ");
+	scanf("%d %d",&num1,&num2);
+	res=num1+num2;
+	printf("Addition is = %d",res);
+}
+
+
