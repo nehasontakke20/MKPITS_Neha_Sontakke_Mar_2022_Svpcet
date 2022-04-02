2257d25893c89c01aa904acd3b9540b062edede2
nehasontakke20 <nishantsontakke2801@gmail.com>
2022-04-02 20:02:52 +0530

    density formula

---
+++ C program/density formula.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float d,m,v;
+	printf("Formula for density = m/v \n");
+	printf("Where d=density, m=mass,v=volume \n");
+	printf("Enter value Of Mass : ");
+	scanf("%f",&m);
+	printf("Enter value Of Volume : ");
+	scanf("%f",&v);
+	d=m/v;
+	printf("Density is : %f",d);
+	
+
+}
+
+
+
+
+
+
