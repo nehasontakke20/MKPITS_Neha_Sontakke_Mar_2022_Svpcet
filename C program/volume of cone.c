a4f1a999f8bf27f0ca15d8ec8315afc8f3c6add7
nehasontakke20 <nishantsontakke2801@gmail.com>
2022-04-02 20:09:15 +0530

    volume of cone

---
+++ C program/volume of cone.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float b,h,v;
+	printf("Enter radius : ");
+	scanf("%f",&b);
+	printf("Enter height : ");
+	scanf("%f",&h);
+	v=b*h/3;
+	printf("Volume of cone is %f",v);
+}
+
+
+
+
+
+
+
+
