5901f8e2cbdac7878da272ea62615c95eabe0453
nehasontakke20 <nishantsontakke2801@gmail.com>
2022-04-02 20:07:45 +0530

    tangentil velocity

---
+++ C program/tangential velocity.c
@@ -0,0 +1,21 @@
+
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float pi=3.14;
+	float r,t,v;
+	printf("Enter value of radius: ");
+	scanf("%f",&r);
+	printf("Enter value of t: ");
+	scanf("%f",&t);
+	v=2*pi*r/t;
+	printf("tangential Velocity is %f",v);
+}
+
+
+
+
+
+
+
