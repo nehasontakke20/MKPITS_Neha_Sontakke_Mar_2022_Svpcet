a92552785a2d4bdd2b6037bdd3ff6afeb2a13354
nehasontakke20 <nishantsontakke2801@gmail.com>
2022-04-02 20:01:18 +0530

    cone of surface

---
+++ C program/cone of surface.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float pi=3.147;
+	float r,l,a;
+	printf("Formula for cone surface area = pi*r*l \n");
+	printf("Where r=radius \n l=length \n");
+	printf("Enter value Of radius : ");
+	scanf("%f",&r);
+	printf("Enter value Of length : ");
+	scanf("%f",&l);
+	a=pi*r*l;
+	printf("%f",a);
+}
+
+
+
+
+
