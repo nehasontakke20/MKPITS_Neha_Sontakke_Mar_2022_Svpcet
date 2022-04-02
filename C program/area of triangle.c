8e71321718d1df525b849c06bb3300eaf8c31260
nehasontakke20 <nishantsontakke2801@gmail.com>
2022-04-02 19:59:31 +0530

    area of triangle

---
+++ C program/area of triangle.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int b,h,a;
+	printf("Enter base value :");
+	scanf("%d",&b);
+	printf("Enter value of height");
+	scanf("%d",&h);
+	a=b*h;
+	printf("Area of Triangle is %d",a);
+	
+}
+
+
+
+
