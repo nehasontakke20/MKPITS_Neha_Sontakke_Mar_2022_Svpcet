9e7473d0fb321f2f4fbcef091c45ed54532c8782
nehasontakke20 <nishantsontakke2801@gmail.com>
2022-04-02 20:06:06 +0530

    percentage formula

---
+++ C program/percentage formula.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float a,b,c;
+	printf("Formula for Percentage : (value/total value)*100 \n");
+	printf("Where a=value \n b=total value \n");
+	printf("Enter value : ");
+	scanf("%f",&a);
+	printf("Enter total value : ");
+	scanf("%f",&b);
+	c=(a/b)*100;
+	printf("%f",c);
+}
+
+
+
+
+
+
+
