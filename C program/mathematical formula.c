ecdf0d088d57aa4c0161a9a1e018d4cea0b96d69
nehasontakke20 <nishantsontakke2801@gmail.com>
2022-04-02 20:11:14 +0530

    mathematical formula

---
+++ C program/mathematical formula.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a,b,c;
+	printf("Formula for (a+b)^2=a*a+b*b+2ab \n");
+	printf("Enter value a: ");
+	scanf("%d",&a);
+	printf("Enter value Of b : ");
+	scanf("%d",&b);
+	c=(a*a)+(b*b)+(2*a*b);
+	printf("(a+b)^2 : %d",c);
+	
+
+}
+
+
+
+
+
+
+
+
+
