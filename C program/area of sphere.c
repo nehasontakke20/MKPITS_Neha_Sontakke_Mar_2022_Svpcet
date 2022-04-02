011921b00b31dc1432454ada127c4289f606b8d2
nehasontakke20 <nishantsontakke2801@gmail.com>
2022-04-02 19:57:47 +0530

    area of sphere

---
+++ C program/area of sphere.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float pi=3.14;
+	float r,s;
+	printf("Enter Value of radius : ");
+	scanf("%f",&r);
+	s=4*pi*r*r;
+	printf("Area of sphere is %f ",s);
+}
+
+
+
+
+
