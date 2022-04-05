28a321a8b2378c19a71d044c3f094f487843a913
nehasontakke20 <nishantsontakke2801@gmail.com>
2022-04-05 21:42:58 +0530

    height and width

---
+++ C program/heigth and width.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+		int height,width,area,perimeter;
+height=7;
+width=5;
+perimeter = 2*(height+width);
+printf("perimeter of rectangle=%d inches\n",perimeter);
+area = height*width;
+printf("area of rectangle=%d square inches\n",area);
+}
