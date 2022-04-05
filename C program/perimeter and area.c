5dd96006dc4e87bc4bfc022a6bf8b005cbe2231e
nehasontakke20 <nishantsontakke2801@gmail.com>
2022-04-05 21:44:32 +0530

    perimeter and area

---
+++ C program/perimeter and area.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int radius;
+	float area,perimeter;
+	radius=6;
+	perimeter=2*3.14*radius;
+	printf("perimeter of the circle=%f inches\n",perimeter);
+	area= 3.14*radius*radius;
+	printf("area of the circle=%f square inches\n", area) ;
+}
