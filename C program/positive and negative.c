4a2bafe72aa279363f65251729d3727e1dec96ea
nehasontakke20 <nishantsontakke2801@gmail.com>
2022-04-07 16:57:45 +0530

    positive and negative

---
+++ C program/positive and negative.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int num;
+printf("input a number:");
+scanf("%d",&num);
+if (num>=0)	
+printf("%d is a positive number \n",num);
+else
+printf("%d is a negative number \n",num);
+}
