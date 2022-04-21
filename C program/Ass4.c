83ee9d4c09dd1daec5e756862b48cf62f1068bfe
nehasontakke20 <nishantsontakke2801@gmail.com>
2022-04-21 10:48:29 +0530

    Ass4

---
+++ C program/Ass4.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int r,c;
+	for(r=1;r<=4;r++)
+	{
+		for(c=1;c<=r;c++)
+	{
+		printf("%d",c);
+	}
+	printf("ABCD");
+}
+{
+	printf("\n");
+}
+	
+}
