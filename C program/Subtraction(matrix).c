33e46762ae49c3f4b33da0ad350ca9bae686baf5
nehasontakke20 <nishantsontakke2801@gmail.com>
2022-04-25 22:22:50 +0530

    Substraction matrix

---
+++ C program/Subtraction(matrix).c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int m1[2][2],m2[2][2],res[2][2],i,j;
+	printf("Enter 2x2 matrix m1\n:");
+	for(i=0;i<=1;i++)
+	{
+		for(j=0;j<=1;j++)
+		{
+			printf("m1[%d][%d]=",i,j);
+			scanf("%d",&m1[i][j]);
+		}
+	}
+	printf("Enter 2x2 matrix m2\n:");
+	for(i=0;i<=1;i++)
+	{
+		for(j=0;j<=1;j++)
+		{
+			printf("m2[%d][%d]=",i,j);
+			scanf("%d",&m2[i][j]);
+		}
+	}
+		for(i=0;i<=1;i++)
+		{
+			for(j=0;j<=1;j++)
+			{
+				res[i][j]=m1[i][j]-m2[i][j];
+			}
+		}
+		//display
+		for(i=0;i<=1;i++)
+		{
+			for(j=0;j<=1;j++)
+			{
+				printf("\nres[%d][%d]=%d",i,j,res[i][j]);
+			}
+		}
+	
+}
