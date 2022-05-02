b266a33d5650e3a99f36fc105399803a537bb3bf
nehasontakke20 <nishantsontakke2801@gmail.com>
2022-05-02 10:16:07 +0530

    no argument no return type(switch)

---
+++ C program/no argument no retur type(switch).c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void add();
+void main()
+{
+	add();
+}
+void add()
+{
+	int choice;
+	printf("enter 1 or 2 or 3");
+	scanf("%d",&choice);
+	switch(choice)
+	{
+		case 1:
+		printf("in case1");
+		break;
+		case 2:
+		printf("in case2");
+		break;
+		case 3:
+		printf("in case3");
+		break;
+	    default:
+		printf("i am in default");
+		}
+}
