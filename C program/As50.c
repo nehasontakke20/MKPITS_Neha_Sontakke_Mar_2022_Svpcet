5b9493e1acc90650143854123f20e4abfc532330
nehasontakke20 <nishantsontakke2801@gmail.com>
2022-05-02 11:23:07 +0530

    Ass 50

---
+++ C program/As50.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	float inch,cm;
+	printf("Enter Distance in cm:\n");
+	scanf("%f",&cm);
+	inch=cm/2.54;
+	printf("distance in inch is %f",inch);
+}
