abe11b672e09f75fe2135205b7e56aeb55c7ca88
nehasontakke20 <nishantsontakke2801@gmail.com>
2022-05-03 22:00:55 +0530

    Ass 73

---
+++ C program/Ass 73.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num1,num2,num3,res;
+	printf("Enter any three number:");
+	scanf("%d%d%d",&num1,&num2,&num3);
+	res=num1*num2*num3;
+	printf("\nmultiplication is =%d",res);
+}
