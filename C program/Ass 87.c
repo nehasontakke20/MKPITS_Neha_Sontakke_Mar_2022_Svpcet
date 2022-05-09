7196362649c26a06d735241fb9d6f6e6c6303501
nehasontakke20 <nishantsontakke2801@gmail.com>
2022-05-09 12:03:00 +0530

    Ass 87

---
+++ C program/Ass 87.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int x,y;
+	printf("\nEnter the integers:");
+	
+	scanf("%d%d",&x,&y);
+	if(x==y)
+	{	
+		printf("\nsame digit");	
+	}
+	else
+	{
+		printf("\nnot same digit");	
+	}
+}
+
