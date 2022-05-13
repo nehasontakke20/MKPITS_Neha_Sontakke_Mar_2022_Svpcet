88f7fec23923f8a44cf252ed490d3c04b6e39385
nehasontakke20 <nishantsontakke2801@gmail.com>
2022-05-13 21:38:41 +0530

    Ass 321

---
+++ C program/Ass321.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int r,sp,p,no,n;
+	printf("Enter the number of rows:");
+	scanf("%d",&no);
+	n=no;
+	for(r=1;r<=no;r++)
+	{
+		for(sp=1;sp<=n;sp++)
+		{
+			printf(" ");
+		}
+		n--;
+		for(p=1;p<=r;p++)
+		{
+			printf("* ");
+		}
+		printf(" \n");
+	}
+}
