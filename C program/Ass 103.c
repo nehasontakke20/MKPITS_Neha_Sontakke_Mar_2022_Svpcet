040e0f9a3f7c4e77881122cf482f89a6a8c83b46
nehasontakke20 <nishantsontakke2801@gmail.com>
2022-05-24 20:17:28 +0530

    Ass 103

---
+++ C program/Ass 103.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,j,a[4];
+	for(i=0;i<4;i++)
+	{
+		printf("Enter any element:");
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<4;i++)
+	{
+		if(i==0)
+		{
+			j=a[i];
+		}
+		a[i]=a[i+1];
+		if(i==3)
+		{
+			a[i]=j;
+		}
+	}
+	for(i=0;i<4;i++)
+	{
+		printf("%d",a[i]);
+	}
+}
