cd1b867ac1f6c05b3bf930be0ae609eedbd4e2f6
nehasontakke20 <nishantsontakke2801@gmail.com>
2022-05-25 16:08:19 +0530

    Ass 68

---
+++ C program/Ass 68.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int num;
+	printf("print number square and cube:");
+	for(num=0;num<=20;num++)
+	printf("%d\t%d\t%d\n",num,num*num,num*num*num);
+}
